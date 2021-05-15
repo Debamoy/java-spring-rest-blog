801ba71a30c31d1ad6b67b2a8533dedfec217f30
Debamoy <datta.debamoy@gmail.com>
2021-05-15 09:37:10 +0530

    completed module 3

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -11,11 +11,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
+import com.pluralsight.blog.data.AuthorRepository;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
 
     private final PostRepository postRepository;
+    private final AuthorRepository authorRepository;
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
     private final String[] gadgets = {
@@ -23,19 +25,29 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
     @Autowired
-    public DatabaseLoader(PostRepository postRepository) {
+    public DatabaseLoader(PostRepository postRepository,AuthorRepository authorRepository) {
         this.postRepository=postRepository;
+        this.authorRepository=authorRepository;
     }
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        authors.addAll(Arrays.asList(
+                new Author("sholderness", "Sarah",  "Holderness", "password"),
+                new Author("tbell", "Tom",  "Bell", "password"),
+                new Author("efisher", "Eric",  "Fisher", "password"),
+                new Author("csouza", "Carlos",  "Souza", "password")
+        ));
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
-
             String title = String.format(template, gadget);
+            Author author=authors.get(i%authors.size());
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
+            author.addPost(post);
+            authors.add(author);
             randomPosts.add(post);
         });
         postRepository.saveAll(randomPosts);
+        authorRepository.saveAll(authors);
     }
 }
--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -5,12 +5,15 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
+@RepositoryRestResource(exported = false)
 public interface PostRepository extends JpaRepository<Post,Long>{
     @RestResource(rel="contains-title", path="containsTitle")
     List<Post> findByTitleContaining(String title);
+    List<Post> findByAuthor_Lastname(String lastName);
 }
